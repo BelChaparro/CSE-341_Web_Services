4fd2eec6fa6ca777118d6276c89cbe037a125740
BelChaparro <a.belenchaparro@gmail.com>
2023-04-22 15:31:01 -0300

    added lesson1 controller

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const antuanRoute = (req,res) => {
+    res.send("Antuán Vidkra");
+  }
+
+const ferRoute = (req,res) => {
+    res.send("Fernando Daniel");
+}
+
+module.exports = {
+    antuanRoute,
+    ferRoute
+};
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Express Web Server:
 const express = require('express');
 const app = express();
+const lesson1Controller = require("./controllers/lesson1");
 
-app.get('/', (req,res) => {
-  res.send("Antuán Chaparro");
-});
+app.get('/', lesson1Controller.antuanRoute);
+
+app.get('/fer', lesson1Controller.ferRoute);
 
 const port = 3000;
+
 app.listen(process.env.port || port);
 console.log('Web Server is listening at port '+ (process.env.port || port));
